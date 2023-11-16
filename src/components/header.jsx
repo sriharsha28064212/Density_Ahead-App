02b3b44d9c4e2a1efe82d803077f5b6c76dd4a2a
sriharsha28064212 <harshakss23@gmail.com>
2023-11-17 04:19:18 +0530

    Header fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,10 +7,10 @@ function Header(){
         <Image src='/head.png' width={80} height={90} alt="logo"/>
         <nav>
           <ul>
-            <li><a href="#section1">Emotions</a></li>
-            <li><a href="#section2">Manifesto</a></li>
-            <li><a href="#section3">Self-awareness Test</a></li>
-            <li><a href="#section4">Work With Us</a></li>
+            <li><a href="#section3">Emotions</a></li>
+            <li><a href="#section5">Manifesto</a></li>
+            <li><a href="#section8">Self-awareness Test</a></li>
+            <li><a href="#section9">Work With Us</a></li>
           </ul>
           <button className='btn btn-md btn-dark' style={{ borderRadius: '20px' }}>Download App</button>
         </nav>
